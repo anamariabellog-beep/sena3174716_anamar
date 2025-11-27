ffd9113a945172b89b829729982eb60c54519fb7
Aprendiz Noche <Aprendiz Noche@YPLAPRSSVFSW017.FormacionSena.RED>
2025-11-26 21:21:08 -0500

    se elimino ejecutar programas.bat

--- Ejecutar programas.bat
+++
@@ -1,6 +0,0 @@
-echo Iniciando programas...
-start "" "C:\Users\%USERNAME%\AppData\Local\Programs\Microsoft VS Code\Code.exe"
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
-start "" "C:\Users\%USERNAME%\AppData\Local\Programs\Microsoft VS Code\Code.exe" --new-window --user-data-dir %TEMP%\vscode-temp
-start "" "C:\Program Files\Adobe\Acrobat DC\Acrobat\Acrobat.exe"
-echo programas abiertos
